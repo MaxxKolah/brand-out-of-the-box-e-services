96d64011194354acb6bd6825735d6133330c2eb5
MaxxKolah <140779767+MaxxKolah@users.noreply.github.com>
2023-11-10 14:01:08 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,42 +1,71 @@
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-}
-
-        int[] data = new int[10];
-
-            System.out.println("Random Number: " + item);
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-public class RandomDataGenerator {
-    }
-    public static void main(String[] args) {
-        }    });
+function main() {
+    });
         console.log("Random Number: " + item);
-
-}
-    for (let i = 0; i < 10; i++) {
-    data.forEach(item => {
-
-    }
-}
-main();
         data.push(Math.floor(Math.random() * 100) + 1);
-    const data = [];
 
+    for (let i = 0; i < 10; i++) {
+    const data = [];
     const data = generateRandomData();
     return data;
-function main() {
 function generateRandomData() {
-def generate_random_data():
-def main():
+}
+main();
 
-    data = [random.randint(1, 100) for _ in range(10)]
+
+    data.forEach(item => {
+}
+    }    data = [random.randint(1, 100) for _ in range(10)]
+
+
         print(f"Random Number: {item}")
+def generate_random_data():
+import random
+    main()
+    for item in data:
+def main():
+if __name__ == "__main__":
+    return data
+
+
+    data = generate_random_data()        print(f"Random Number: {item}")
+    for item in data:
+    return data
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
     data = generate_random_data()
 
 if __name__ == "__main__":
-import random
-    return data
 
+
+
+    main()
+def main():}
+    const data = [];
+    data.forEach(item => {
+    return data;
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+main();
+    const data = generateRandomData();
+
+
+    });
+    for (let i = 0; i < 10; i++) {
+function main() {
+}
+
+        console.log("Random Number: " + item);    return data
+import random
+
+
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+if __name__ == "__main__":
     for item in data:
+        print(f"Random Number: {item}")
+def main():
     main()
