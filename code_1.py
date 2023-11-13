8477435f8ad7cb746c098d4a12d984d59dc2d65d
MaxxKolah <140779767+MaxxKolah@users.noreply.github.com>
2023-11-13 09:03:26 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,30 +1,30 @@
-    data = generate_random_data()
+    const data = [];
+function generateRandomData() {
+
+
+}
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+    });
+    const data = generateRandomData();
+    return data;
+    }
+
+main();
+        console.log("Random Number: " + item);
+}    main()
+
     for item in data:
-def main():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
 import random
-    return data
+def main():
 
     data = [random.randint(1, 100) for _ in range(10)]
 
-
-    main()
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-
-def generate_random_data():    return data;
-        data.push(Math.floor(Math.random() * 100) + 1);
-
-function main() {
-}
-
-        console.log("Random Number: " + item);
-    });
-}
-
-    }
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-main();
-function generateRandomData() {
-    data.forEach(item => {
-    const data = generateRandomData();
+def generate_random_data():
+    return data
